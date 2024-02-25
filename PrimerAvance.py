d05f488742db28fd49545b79f5d609ecd70460db
OutaTime <joseignaciocastrog@gmail.com>
2024-02-24 20:32:50 -0600

    Portada en notas de codigo
    Resuelto con un merge.

--- PrimerAvance.py
+++ PrimerAvance.py
@@ -1,3 +1,10 @@
+# Universidad Fidélitas.
+# Proyecto de programación basica.
+# Curso: SC-202, Programacion basica (Introducción a la programación). 
+# Tema del proyecto: Academia de manejo.
+# Grupo #2
+# Integrantes: Jose Castro. Juan Castro. Luna Castillo. Scarlet Villamizar.
+
 print("Este es el sistema de la academia de manejo Fidélitas.")
 
 #Entradas
