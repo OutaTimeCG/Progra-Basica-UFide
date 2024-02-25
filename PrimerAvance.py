4ae0bdc9d0b24b719754c272b0684ee4b39b0af1
Escarlata0 <scar06.04.2005@gmail.com>
2024-02-24 20:07:25 -0600

    Update PrimerAvance.py
    Enunciado

---
+++ PrimerAvance.py
@@ -0,0 +1,7 @@
+# Universidad Fidélitas.
+# Proyecto de programación basica.
+# Curso: SC-202, Programacion basica (Introducción a la programación). 
+# Tema del proyecto: Academia de manejo.
+# Grupo #2
+# Integrantes: Jose Castro. Juan Castro. Luna Castillo. Scarlet Villamizar.
+ 
